54a5f28c49dfcda2e7158042a9751507a8abda5a
deanefraim1 <deanefraim@campus.technion.ac.il>
2023-04-14 18:35:36 +0300

    .

--- pyRDDLGym/Policies/Agents.py
+++ pyRDDLGym/Policies/Agents.py
@@ -13,7 +13,7 @@ C_UCB_PARAM = np.sqrt(2)
 class BaseAgent(metaclass=ABCMeta):
 
     @abstractmethod
-    def sample_action(self, state):
+    def sample_action(self, state, env):
         pass
 
 
@@ -24,7 +24,7 @@ class RandomAgent(BaseAgent):
         if seed is not None:
             self.action_space.seed(seed)
 
-    def sample_action(self, state=None):
+    def sample_action(self, state=None, env=None):
         s = self.action_space.sample()
         action = {}
         selected_actions = random.sample(list(s), self.num_actions)
@@ -54,15 +54,14 @@ class MCTSAgent(BaseAgent):
     def __init__(self, action_space, num_actions=1, seed=None):
         self.action_space = action_space
         self.num_actions = num_actions
-        #FIXME: WHAT IS SEED USED FOR?
         if seed is not None:
             self.action_space.seed(seed)
 
-    def sample_action(self, state):
+    def sample_action(self, state, env):
         s = self.action_space.sample()
         action = {}
-        selected_actions = self.__GetActionWithMCTS(state)
-        #FIXME - what we need the for loop for?
+        selected_actions = self.__GetActionWithMCTS(state, env)
+        #NOTE - what we need the for loop for?
         for sample in selected_actions:
             if isinstance(self.action_space[sample], gym.spaces.Box):
                 action[sample] = s[sample][0].item()
@@ -70,26 +69,15 @@ class MCTSAgent(BaseAgent):
                 action[sample] = s[sample]
         return action
     
-    def __GetActionWithMCTS(self, initial_state):
-        #NOTE - flow-on-link___l<i>__t<j> => how many cars in link <i> at time <j>, when 0<=i<=7, 0<=j<=20
-        #NOTE - q__l<i>__l<j> =>how many cars are waiting in the queue (t0) to go from link <i> to link <j>, when 0<=i<=7, 0<=j<=7
-        #NOTE - Nc___l<i> => how many cars are in link <i> (sum all times), when 0<=i<=7
-        #NOTE - virtual-q___l<i> => 
-        #NOTE - signal___i0 => the current signl state of i0, when 0<=i<=7
-        #NOTE - signal-t___i0 => the time that the intersection i0 has been in the current signal state, when 0<=i<=7
-        #NOTE - len() => 
-        root = MonteCarloTreeSearchNode(state = initial_state)
-        return root.best_action().parent_action
+    def __GetActionWithMCTS(self, initial_state, env):
+        root = MonteCarloTreeSearchNode(env = env,
+                                        state = initial_state,
+                                        action_space = self.action_space)
+        return root.best_action()
 
 class MonteCarloTreeSearchNode():
-    def __init__(self, env, inst, state, action_space, score = 0, _is_terminal_node = False, parent=None, parent_action=None):
-        EnvInfo = ExampleManager.GetEnvInfo(env)
-        self._myEnv = RDDLEnv.RDDLEnv(domain=EnvInfo.get_domain(), 
-                            instance=EnvInfo.get_instance(inst),
-                            enforce_action_constraints=False,
-                            debug=False,
-                            log=False)
-        self._inst = inst
+    def __init__(self, env, state, action_space, score = 0, parent=None, parent_action=None):
+        self._myEnv = env
         self._state = state
         self._parent = parent
         self._parent_action = parent_action
@@ -98,19 +86,21 @@ class MonteCarloTreeSearchNode():
         self._score = score
         self._untried_actions = action_space
         self._action_space = action_space
-        self._is_terminal_node = _is_terminal_node
         return
     
     def expand(self):
-        action = self._untried_actions.pop()
+        action = self._untried_actions.pop_random_action() #TODO - find a way to pop a random item from the ordered dict of spaces
         new_env = self._myEnv
         next_state, reward, done, info = new_env.step(action)
-        child_node = MonteCarloTreeSearchNode(new_env, self._inst, next_state, self._action_space, reward, done, parent=self, parent_action=action)
+        child_node = MonteCarloTreeSearchNode(env = new_env,
+                                              state = next_state,
+                                              action_space = self._action_space, 
+                                              score = reward, 
+                                              is_terminal_node = done, 
+                                              parent=self, 
+                                              parent_action=action)
         self.children.append(child_node)
         return child_node
-
-    def is_terminal_node(self):
-        return self._is_terminal_node or self.time_remaining == 0
     
     def rollout(self):
         current_myEnv = self._myEnv
@@ -150,4 +140,4 @@ class MonteCarloTreeSearchNode():
             v = self._tree_policy()
             reward = v.rollout()
             v.backpropagate(reward)
-        return self.best_child()
+        return self.best_child().parent_action
